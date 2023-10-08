b880cc9c495442bead98d00b635e1a83838cf011
Gustavo Maia <vm.gustavo31@gmail.com>
2023-10-08 17:18:28 -0300

    update exec.sh

--- tp1/src/exec.sh
+++ tp1/src/exec.sh
@@ -3,7 +3,8 @@
 python run.py \
   --multirun \
   hydra/launcher="joblib" \
-  n_episodes="20000" \
+  ++hydra.launcher.n_jobs="10" \
+  n_episodes="25000" \
   n_observations="range(0, 30)" \
   qtable.alpha="0.001,0.005,0.01,0.05,0.1,0.2,0.4" \
-  qtable.gamma="0.0,0.3,0.7,0.9,1.0"
+  qtable.gamma="0.0,0.1,0.3,0.5,0.7,0.9,1.0"
