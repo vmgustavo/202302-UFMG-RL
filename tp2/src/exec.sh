cf280653d83c32627d3741ff98c0066c453feddc
Gustavo Maia <vm.gustavo31@gmail.com>
2023-12-02 12:20:03 -0300

    update exec.sh

--- tp2/src/exec.sh
+++ tp2/src/exec.sh
@@ -1,12 +1,16 @@
 # https://hydra.cc/docs/advanced/override_grammar/extended/
 
 python run-inverted-double-pendulum.py \
-  model_params.batch_size="128" \
-  model_params.gamma="0.99" \
-  model_params.eps_start="0.9" \
-  model_params.eps_end="0.05" \
-  model_params.eps_decay="10000" \
-  model_params.tau="0.005" \
-  model_params.lr="1e-4" \
-  model_params.memory_size="10000" \
-  n_episodes="200"
+  --multirun \
+  hydra/launcher="joblib" \
+  ++hydra.launcher.n_jobs="2" \
+  interactive.plot="false" \
+  model_params.batch_size="128,256" \
+  model_params.gamma="0.95,0.99" \
+  model_params.eps_start="0.9,0.99" \
+  model_params.eps_end="0.01,0.05" \
+  model_params.eps_decay="10000,20000" \
+  model_params.tau="0.005,0.01" \
+  model_params.lr="1e-1,1e-4" \
+  model_params.memory_size="5000,10000" \
+  n_episodes="500"
